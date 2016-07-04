da1a99c3f25645914986046894460ab05660db40
Dale Stone <dales@dotnetit.co.uk>
2016-07-04 20:33:34 +0100

    Added square service.

---
+++ src/ProjectEulerService/SquareService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectEulerService
+{
+    public class SquareService
+    {
+        public int SumOfSquares(int n)
+        {
+            var sum = 0;
+
+            for (var i = 1; i <= n; i++)
+            {
+                sum += (int)Math.Pow(i, 2);
+            }
+
+            return sum;
+        }
+
+        public int SquareOfSum(int n)
+        {
+            var sum = 0;
+            var sumSquared = 0;
+
+            for (var i = 1; i <= n; i++)
+            {
+                sum += i;
+            }
+
+            sumSquared = (int)Math.Pow(sum, 2);
+
+            return sumSquared;
+        }
+
+        public int SumSquareDifference(int n)
+        {
+            return SquareOfSum(n) - SumOfSquares(n);
+        }
+    }
+}
---
+++ test/ProjectEulerServices.Tests/SquareServiceTests.cs
@@ -0,0 +1,59 @@
+﻿using ProjectEulerService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ProjectEulerServices.Tests
+{
+    public class SquareServiceTests
+    {
+        private readonly SquareService _squareService;
+
+        public SquareServiceTests()
+        {
+            _squareService = new SquareService();
+        }
+
+        [Fact]
+        public void ReturnSumOfSquares()
+        {
+            var n = 10;
+            var expected = 385;
+            var actual = _squareService.SumOfSquares(n);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ReturnSquareOfSum()
+        {
+            var n = 10;
+            var expected = 3025;
+            var actual = _squareService.SquareOfSum(n);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ReturnSumSquareDifference()
+        {
+            var n = 10;
+            var expected = 2640;
+            var actual = _squareService.SumSquareDifference(n);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ReturnProjectEulerProblem6Answer()
+        {
+            var n = 100;
+            var expected = 25164150;
+            var actual = _squareService.SumSquareDifference(n);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
